f84f592ebc0b9f5e96f5ba75cd84e4e6d1e60b9c
dodaa08 <dodakartik26@gmail.com>
2025-05-22 20:51:16 +0530

    added the dark mode

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
       >
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
         <AppWalletProvider>
           <Header />
           <main className="flex-grow">{children}</main>
